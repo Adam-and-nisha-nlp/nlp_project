c8ac7174a0dddbac6479190953c33b285de6c102
Thoai Adam Nguyen <nguyenphithoaihung@gmail.com>
2023-10-24 00:18:52 -0500

    finished power point

---
+++ final_wrangle.py
@@ -0,0 +1,373 @@
+# WRANGLE
+
+import requests
+import json
+
+def acquire_github_repositories(language="python", per_page=100):
+    # Replace these with your own GitHub token and username
+    github_token = "your_github_token"
+    github_username = "your_github_username"
+
+    # Set up API request headers
+    headers = {
+        "Authorization": f"token {github_token}",
+        "User-Agent": github_username
+    }
+
+    # Make API requests to get repository names
+    api_url = f"https://api.github.com/search/repositories?q=language:{language}&sort=stars&order=desc&per_page={per_page}"
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        repositories = response.json()["items"]
+        return repositories
+    else:
+        print(f"Failed to fetch repository list. Status code: {response.status_code}")
+        return []
+
+# Example usage:
+python_repositories = acquire_github_repositories(language="python", per_page=100)
+
+
+#----------------------------------------------------------------------------------
+
+
+def breast_cancer_repository(query="breast cancer", max_pages=10):
+    # Replace these with your own GitHub token and username
+    github_token = "your_github_token"
+    github_username = "your_github_username"
+
+    # Define the headers with your token and username
+    headers = {
+        "Authorization": f"token {github_token}",
+        "User-Agent": github_username
+    }
+
+    # Define a function to get repositories based on a query and page
+    def get_repositories(query, sort="stars", order="desc", page=1):
+        base_url = "https://api.github.com/search/repositories"
+        params = {
+            "q": query,
+            "sort": sort,
+            "order": order,
+            "per_page": 10,  # You can get up to 100 results per page.
+            "page": page  # Specify the page number
+        }
+        response = requests.get(base_url, headers=headers, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    # Create a list to collect the repository data
+    repository_data = []
+
+    page = 1
+    while page <= max_pages:
+        response_json = get_repositories(query, page=page)
+        if response_json is None:
+            print(f"Failed to fetch page {page} of repositories.")
+            break
+
+        # Extract and collect repository names
+        for repo in response_json["items"]:
+            repository_data.append({
+                "full_name": repo["full_name"]
+            })
+
+        # Check if there are more pages
+        if "Link" in response.headers:
+            next_link = response.headers["Link"]
+            if 'rel="next"' not in next_link:
+                break
+        else:
+            break
+        page += 1
+
+    # Extract only the repository names from the dictionaries
+    repository_names = [repo_data["full_name"] for repo_data in repository_data]
+
+    # Save the repository names list to a JSON file
+    with open("breast_cancer_repository.json", "w") as json_file:
+        json.dump(repository_names, json_file, indent=2)
+
+    print("Data saved to breast_cancer_repository.json")
+
+# Example usage:
+# breast_cancer_repository(query="breast cancer", max_pages=10)
+
+
+
+import requests
+import json
+from env import github_token, github_username
+
+def get_repository_details(repo_full_name):
+    repo_url = f"https://api.github.com/repos/{repo_full_name}"
+    readme_url = f"https://api.github.com/repos/{repo_full_name}/readme"
+    repo_response = requests.get(repo_url, headers=headers)
+    readme_response = requests.get(readme_url, headers=headers)
+    if repo_response.status_code == 200 and readme_response.status_code == 200:
+        repo_data = repo_response.json()
+        readme_data = readme_response.json()
+        language = repo_data["language"]
+        readme_contents = requests.get(readme_data["download_url"]).text
+        return {
+            "repo": "/" + repo_full_name,
+            "language": language,
+            "readme_contents": readme_contents
+        }
+    else:
+        return None
+
+def get_repositories(query, sort="stars", order="desc", page=1, max_pages=10):
+    base_url = "https://api.github.com/search/repositories"
+    all_repos = []
+
+    while page <= max_pages:
+        params = {
+            "q": query,
+            "sort": sort,
+            "order": order,
+            "per_page": 10,  # You can get up to 100 results per page.
+            "page": page  # Specify the page number
+        }
+        response = requests.get(base_url, headers=headers, params=params)
+
+        if response.status_code != 200:
+            print(f"Failed to fetch page {page} of repositories.")
+            break
+
+        response_json = response.json()
+        all_repos.extend(response_json["items"])
+        
+        page += 1
+
+        if "Link" in response.headers:
+            next_link = response.headers["Link"]
+            if 'rel="next"' not in next_link:
+                break
+
+    return all_repos
+
+def scrape_breast_cancer_repositories():
+    # Search for breast cancer related repositories and handle pagination
+    query = "breast cancer"
+    page = 1
+    max_pages = 10
+
+    breast_cancer_repos = get_repositories(query, page=page, max_pages=max_pages)
+
+    with open("breast_cancer_repositories.json", "w") as json_file:
+        json.dump(breast_cancer_repos, json_file, indent=4)
+
+    return breast_cancer_repos
+
+# Example usage:
+if __name__ == "__main__":
+    scraped_repositories = scrape_breast_cancer_repositories()
+    print(f"Scraped {len(scraped_repositories)} breast cancer repositories.")
+
+# wrangle the data
+import pandas as pd
+import numpy as np
+
+# see the data
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# play with words
+import unicodedata
+from nltk.corpus import stopwords
+import nltk
+import re
+from pprint import pprint
+
+# split and model
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# import of stats testing
+from scipy import stats
+from scipy.stats import chi2_contingency
+
+# import warning
+import warnings
+warnings.filterwarnings('ignore')
+
+#----------------------------------------------------------------------------------
+
+
+# PREPARE
+
+### functions to get the data###
+### functions to prep the data ###
+
+def basic_clean(original):
+    '''
+    Input: original text or .apply(basic_clean) to entire data frame
+    Actions: 
+    lowercase everything,
+    normalizes everything,
+    removes anything that's not a letter, number, whitespace, or single quote
+    Output: Cleaned text
+    '''
+    # lowercase everything
+    basic_cleaned = original.lower()
+    # normalize unicode characters
+    basic_cleaned = unicodedata.normalize('NFKD', basic_cleaned)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8', 'ignore')
+    # Replace anything that is not a letter, number, whitespace or a single quote.
+    basic_cleaned = re.sub(r'[^a-z0-9\'\s]', '', basic_cleaned)
+    
+    return basic_cleaned
+
+
+def tokenize(basic_cleaned):
+    '''
+    Input: basic_cleaned text string or .apply(tokenize) to entire data frame
+    Actions:
+    creates the tokenizer
+    uses the tokenizer
+    Output: clean_tokenize text string
+    '''
+    #create the tokenizer
+    tokenize = nltk.tokenize.ToktokTokenizer()
+    #use the tokenizer
+    clean_tokenize = tokenize.tokenize(basic_cleaned, return_str=True)
+    
+    return clean_tokenize
+
+
+def remove_stopwords(lemma_or_stem, extra_words=[], exclude_words=[]):
+    '''
+    Input:text string or .apply(remove_stopwords) to entire data frame
+    Action: removes standard stop words
+    Output: parsed_article
+    '''
+    # save stopwords
+    # Got the list of English stopwords
+    stopwords_ls = stopwords.words('english')
+
+    # Added the specified words to the list
+    stopwords_ls.extend(['breast', 'cancer', 'lymph', 'node', 'benign', 'malignant', 'wisconsin', 'diagnostic', 'data','dataset','image','model','analysis'])
+    # removing any stopwords in exclude list
+    
+    stopwords_ls = set(stopwords_ls) - set(exclude_words)
+    # adding any stopwords in extra list
+
+    # split words in article
+    words = lemma_or_stem.split()
+    # remove stopwords from list of words
+    filtered = [word for word in words if word not in stopwords_ls]
+    # join words back together
+    parsed_article = ' '.join(filtered)
+    return parsed_article
+
+
+
+
+
+    
+def lemmatize(clean_tokenize):
+    '''
+    Inputs: clean_tokenize
+    Actions: creates lemmatizer and applies to each word
+    Outputs: clean_tokenize_lemma
+    '''
+    #create the lemmatizer
+    wnl = nltk.stem.WordNetLemmatizer()
+    #use lemmatize - apply to each word in our string
+    lemmas = [wnl.lemmatize(word) for word in clean_tokenize.split()]
+    #join words back together
+    clean_tokenize_lemma = ' '.join(lemmas)
+    
+    return clean_tokenize_lemma
+
+def clean(text):
+    '''
+    A simple function to cleanup text data.
+    
+    Args:
+        text (str): The text to be cleaned.
+        
+    Returns:
+        list: A list of lemmatized words after cleaning.
+    '''
+    
+    # basic_clean() function from last lesson:
+    # Normalize text by removing diacritics, encoding to ASCII, decoding to UTF-8, and converting to lowercase
+    text = (unicodedata.normalize('NFKD', text)
+             .encode('ascii', 'ignore')
+             .decode('utf-8', 'ignore')
+             .lower())
+    
+    # Remove punctuation, split text into words
+    words = re.sub(r'[^\w\s]', '', text).split()
+    
+    
+    # lemmatize() function from last lesson:
+    # Initialize WordNet lemmatizer
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Combine standard English stopwords with additional stopwords
+    stopwords = nltk.corpus.stopwords.words('english')
+    
+    # Lemmatize words and remove stopwords
+    cleaned_words = [wnl.lemmatize(word) for word in words if word not in stopwords]
+    
+    return cleaned_words
+
+def cleaned(df):
+    '''
+    This function will clean the df
+    drop nulls, replace special characters
+    '''
+    # drop nulls
+    df = df.dropna()
+    # replace special characters with space
+    df.readme_contents = df.readme_contents.str.replace('[/,_,-,:,"]', ' ', regex=True)
+    # replace heavy, check, and mark with nothing
+    df.readme_contents = df.readme_contents.str.replace('heavy', '').str.replace('check', '').str.replace('mark', '')
+    # create column with clean text. Tokenized, normalized, lemmatized, stop words removed
+    df['clean_norm_token'] = df.readme_contents.apply(tokenize).apply(basic_clean).apply(remove_stopwords).apply(lemmatize)
+    # replace 124 with nothing. 124 was created by the program removing '|'
+    df.clean_norm_token = df.clean_norm_token.str.replace('124', '')
+    #in language column replace language with other if it is not in the top 5 languages
+    top_5 = df.language.value_counts().head(5).index.tolist()
+    df.language = df.language.apply(lambda x: x if x in top_5 else 'other')
+    
+    return df
+
+### functions to split the data##
+
+def split_function_cat_target(df_name, target_varible_column_name):
+    train, test = train_test_split(df_name,
+                                   random_state=123, #can be whatever you want
+                                   test_size=.20,
+                                   stratify= df_name[target_varible_column_name])
+    
+    train, validate = train_test_split(train,
+                                   random_state=123,
+                                   test_size=.25,
+                                   stratify= train[target_varible_column_name])
+    return train, validate, test
+
+# the test will contain 20% of the data,
+# the validation contain 25%('test_size') of previous train which is 20% of original dataset the same as the test set
+
+def plot_bigrams(language,words):
+    word_data = {k[0] + ' ' + k[1]: v for k, v in words.to_dict().items()}
+    
+    word_img = WordCloud(background_color='white', width=800, height=400).generate_from_frequencies(word_data)
+    
+    plt.figure(figsize=(8, 4))
+    plt.imshow(word_img)
+    plt.axis('off')
+    plt.title(f'Top Words for {language}')
+    plt.show()
+
+
